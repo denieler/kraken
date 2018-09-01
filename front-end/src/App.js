83935e7d6c27af2be1b1ddfe6c09e8db271636ee
Daniel Ostapenko <danielostapenko@gmail.com>
2018-09-01 17:42:33 +0200

    feat: add kraken header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/kraken_logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='header'>
+          <img src={logo} className='header__logo' alt='Kraken' />
+          <div class='header__nav nav'>
+            <a href='/' className='nav__link'>List documents</a>
+            <a href='/' className='nav__link'>Upload</a>
+          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
